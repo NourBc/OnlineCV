e2d0d600c2aa7a89fe578cddca31b9950c9d1022
NourBc <Nourbachraoui123@hotmail.com>
2021-05-17 15:04:06 +0100

    first version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { from } from 'rxjs';
+import { HomepageComponent } from './Homepage/homepage.component';
+import {AboutComponent } from './About/about.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'homepage', component:HomepageComponent },
+  {path:'about', component:AboutComponent},
+  {path:'resume', component:ResumeComponent},
+  {path:'projects', component:ProjectsComponent},
+  {path:'contact', component:ContactComponent},
+  {path:'',component:HomepageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './Homepage/homepage.component';
+import { AboutComponent } from './About/about.component';
+import { PhpComponent } from './php/php.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    AboutComponent,
+    PhpComponent
   ],
   imports: [
     BrowserModule,
